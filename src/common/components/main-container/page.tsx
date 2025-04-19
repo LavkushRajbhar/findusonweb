06463419decad37608dd54f93ce0d32414e37c2c
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2025-04-19 11:21:53 +0530

    feat: refactor code

--- src/common/components/main-container/page.tsx
+++ src/common/components/main-container/page.tsx
@@ -1,16 +1,9 @@
-/** @format */
-
-import React from "react";
-// import Header from "@/common/components/layouts/header";
 import Sidebar from "../sidebar/page";
-import NewHeader from "@/common/components/layouts/header/NewHeader";
-
+import HeaderWrapper from '../layouts/header/HeaderWrapper'
 const MainContainer = () => {
   return (
     <div>
-      <NewHeader />
-      {/* <Header /> */}
-
+      <HeaderWrapper/>
       <Sidebar />
     </div>
   );
